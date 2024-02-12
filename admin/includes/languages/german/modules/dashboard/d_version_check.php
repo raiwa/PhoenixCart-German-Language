603054f768f05b047a8cf0189c3f80ec099a0d13
Rainer Schmied <raiwa@phoenixcartaddons.com>
2024-02-12 17:53:37 +0100

    Update d_version_check.php

--- admin/includes/languages/german/modules/dashboard/d_version_check.php
+++ admin/includes/languages/german/modules/dashboard/d_version_check.php
@@ -15,5 +15,7 @@ const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_DESCRIPTION = 'Zeige das Ergebnis des
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_DATE = 'Zuletzt geprüft';
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_CHECK_NOW = 'Prüfe jetzt';
 
+const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_CURRENT = 'Versionsprüfung %s';
+
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_UPDATE_AVAILABLE = '<i class="fas fa-exclamation-circle"></i> Ein Update für CE Phoenix ist verfügbar!';
 const MODULE_ADMIN_DASHBOARD_VERSION_CHECK_IS_LATEST = '<i class="fas fa-thumbs-up"></i> Dies ist die aktuellste Version von CE Phoenix!';
