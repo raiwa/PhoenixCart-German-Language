1351e3b14e41813ce1cbdac9140b6e7d97bc4703
Rainer Schmied <raiwa@phoenixcartaddons.com>
2024-03-06 14:24:35 +0100

    Update password_forgotten.php
    Fix error in line 25

--- includes/languages/german/password_forgotten.php
+++ includes/languages/german/password_forgotten.php
@@ -22,7 +22,7 @@ const TEXT_PASSWORD_RESET_INITIATED = 'Eine Email wurde an Ihre angegebene Adres
 const TEXT_NO_EMAIL_ADDRESS_FOUND = 'Fehler: Die eingegebene eMail-Adresse ist nicht registriert. Bitte versuchen Sie es noch einmal.';
 
 const EMAIL_PASSWORD_RESET_SUBJECT = STORE_NAME . ' - Neues Passwort';
-const EMAIL_PASSWORD_RESET_BODY = 'Ein neues Passwort für Ihr Konto bei ' . STORE_NAME  . " wurde angefordert\n\n" . 'Folgen Sie diesem Link, um Ihr Passwort sicher zu ändern:' . "\n\n" . '%s' . "\n\n" . 'Dieser Link wird automatisch nach 24 Stunden, oder nachdem Ihr Passwort geändert wurde, verworfen.' . "\n\n" . 'Wenn Sie Hilfe mit einem unserer Online-Dienste benötigen, mailen Sie an: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n");
+const EMAIL_PASSWORD_RESET_BODY = 'Ein neues Passwort für Ihr Konto bei ' . STORE_NAME  . " wurde angefordert\n\n" . 'Folgen Sie diesem Link, um Ihr Passwort sicher zu ändern:' . "\n\n" . '%s' . "\n\n" . 'Dieser Link wird automatisch nach 24 Stunden, oder nachdem Ihr Passwort geändert wurde, verworfen.' . "\n\n" . 'Wenn Sie Hilfe mit einem unserer Online-Dienste benötigen, mailen Sie an: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
 
 const ERROR_ACTION_RECORDER = ' Fehler: Ein Link zum Zurücksetzen des Passworts wurde bereits gesendet. Versuchen Sie es in %s Minuten.';
 const IMAGE_BUTTON_RESET_PASSWORD = 'Passwort ändern';
