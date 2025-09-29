92c18b2c49594818872189ca57a15fdab177cb94
Rainer Schmied <raiwa@phoenixcartaddons.com>
2025-09-29 15:52:23 +0200

    1.1.0.6.r1

--- includes/languages/german/modules/content/footer_suffix/cm_footer_extra_icons.php
+++ includes/languages/german/modules/content/footer_suffix/cm_footer_extra_icons.php
@@ -13,8 +13,8 @@
   const MODULE_CONTENT_FOOTER_EXTRA_ICONS_TITLE = 'Extera Fußzeilen-Icons';
 
   const MODULE_CONTENT_FOOTER_EXTRA_ICONS_DESCRIPTION = <<<'EOT'
-  Fügt FontAwesome-Icons zur extra Fußzeile Ihrer Website hinzu.<div class="alert alert-info">Iconsuche: https://fontawesome.com/v6/search</div>
-  EOT;
+Fügt FontAwesome-Icons zur extra Fußzeile Ihrer Website hinzu.<div class="alert alert-info">Iconsuche: https://fontawesome.com/v6/search</div>
+EOT;
 
   // alternatively you can override Fontawesome using;
   const MODULE_CONTENT_FOOTER_EXTRA_ICONS_TEXT = '';
--- includes/languages/german/modules/pi/index/i_hero.php
+++ includes/languages/german/modules/pi/index/i_hero.php
@@ -14,12 +14,12 @@
   const I_HERO_DESCRIPTION = 'Text in einem Hero-Layout anzeigen.<div class="alert alert-info">Dies ist ein Untermodul zur Verwendung mit dem &pi;-System.</div>';
 
   const I_HERO_TEXT = <<<EOT
-  <h1 class="text-body-emphasis">#deinphoenix #deinegemeinschaft</h1>
-  <p class="col-lg-8 mx-auto fs-5 text-muted">
-    Schnapp es dir, tagge es, teile es. Mach es zu deinem.
-  </p>
-  EOT;
+<h1 class="text-body-emphasis">#deinphoenix #deinegemeinschaft</h1>
+<p class="col-lg-8 mx-auto fs-5 text-muted">
+  Schnapp es dir, tagge es, teile es. Mach es zu deinem.
+</p>
+EOT;
 
   const I_HERO_BUTTONS = <<<EOT
-  <a href="%s" class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" role="button">Testimonials</a>
-  EOT;
+<a href="%s" class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" role="button">Testimonials</a>
+EOT;
