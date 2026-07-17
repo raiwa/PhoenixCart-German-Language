ffc7e2eeb9e7f1c8b8b41a5bb79ac6e05dcc4283
Rainer Schmied <raiwa@phoenixcartaddons.com>
2026-07-17 15:04:12 +0200

    Update pi_model.php

--- includes/languages/german/modules/pi/product_info/pi_model.php
+++ includes/languages/german/modules/pi/product_info/pi_model.php
@@ -13,4 +13,4 @@
   const PI_MODEL_TITLE         = 'Modell';
   const PI_MODEL_DESCRIPTION   = 'Zeigt das Produkt-Modell auf der Produktübersichts-Seite.<div class="alert alert-info">Dies ist ein Untermodul für das &pi;-System.</div>';
   
-  const PI_MODEL_DISPLAY_MODEL = 'Modell:<span class="badge badge-primary badge-pill">%s</span>';
+  const PI_MODEL_DISPLAY_MODEL = 'Modell:<span class="badge rounded-pill text-bg-primary">%s</span>';
